7df2acb329f621de5f641043857454bce437d4c5
unknown <shaman4ik7000@gmail.com>
2023-03-21 22:34:02 +0200

    second push

---
+++ lab_1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,54 @@
+﻿
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string line, new_line, result_line; 
+    char prev_char = '\0';
+
+
+    ifstream file("cmd.txt"); // відкриття файлу для читання
+  
+    if (file.is_open()) { // перевірка, чи вдалося відкрити файл
+        while (getline(file, line)) { // зчитування рядка з файлу у змінну line
+            new_line = "";
+            if (line.length() < 1) { 
+                if (prev_char != '\n') {
+                    new_line = '\n'; 
+                    prev_char = '\n';
+                }
+            }
+
+            for (char& c : line) { // проходження по кожному символу в рядку
+                if (c != ' ' && c != '\t' && c != '\n') { // якщо символ не є пропуском, додати його до нового рядка
+                    new_line += c;
+                }
+                else if (prev_char != ' ' && prev_char != '\t' && prev_char != '\n') { // якщо це перший пропуск підряд, додати один пропуск до нового рядка
+                    new_line += c;
+                }
+                prev_char = c;\
+            }
+
+            result_line += new_line;
+        }
+        file.close(); // закриття файлу
+    }
+    else {
+        return 1;
+    }
+
+    ofstream outfile("cmd.txt", ios::app); // відкриття файлу для запису
+    if (outfile.is_open()) { // перевірка, чи вдалося відкрити файл
+        outfile << "\n" << result_line; // запис нового рядка в файл
+        outfile.close(); // закриття файлу
+    }
+    else {
+        return 1;
+    }
+
+    return 0;
+}
---
+++ lab_2/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,33 @@
+﻿
+#include <iostream>
+#include <fstream>
+#include <string>
+#define VARIANT1
+
+using namespace std;
+
+
+
+
+int main()
+{
+    string sentence1 = "this is string one";
+    string sentence2 = "this is string two";
+    string result;
+   
+#ifdef VARIANT1 
+        result =  sentence1;
+#else 
+        result = sentence2;
+#endif
+
+ofstream file("file.txt");
+
+    if (file.is_open()) {
+        file << result;
+
+        file.close();
+    }  
+    
+    return 1;
+}
