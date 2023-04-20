5c03a553a0b3d56b1302253e72b8a7989d840119
DaMoiKa <shaman4ik7000@gmail.com>
2023-04-21 00:21:18 +0300

    third push

---
+++ lab4/lab4/lab4.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <fstream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    ifstream input("input.txt");
+    ofstream output("output.tmp");
+
+    stack<char> st;
+    char c;
+    int line_number = 1;
+    bool error_found = false;
+
+    while (input.get(c)) {
+        if (c == '\n') {
+            line_number++;
+        }
+        else if (c == '(' || c == '[' || c == '{') {
+            st.push(c);
+        }
+        else if (c == ')' || c == ']' || c == '}') {
+            if (st.empty()) {
+                cout << "Error on line " << line_number << ": unexpected closing bracket " << c << endl;
+                error_found = true;
+                break;
+            }
+
+            char opening_bracket = st.top();
+            if ((opening_bracket == '(' && c == ')') ||
+                (opening_bracket == '[' && c == ']') ||
+                (opening_bracket == '{' && c == '}')) {
+                st.pop();
+            }
+            else {
+                cout << "Error on line " << line_number << ": expected " << (opening_bracket == '(' ? ')' : (opening_bracket == '[' ? ']' : '}')) << " but found " << c << endl;
+                error_found = true;
+                break;
+            }
+        }
+    }
+
+    if (!error_found && !st.empty()) {
+        char opening_bracket = st.top();
+        cout << "Error: " << (opening_bracket == '(' ? ')' : (opening_bracket == '[' ? ']' : '}')) << " expected but end of file found" << endl;
+        error_found = true;
+    }
+
+    if (!error_found) {
+        cout << "No errors found" << endl;
+    }
+
+    input.close();
+    output.close();
+
+    return 0;
+}
